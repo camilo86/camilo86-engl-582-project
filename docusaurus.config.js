977cd229577a49b92529942410e8a916bca44d22
Camilo Gonzalez <cgs.camilogonzalez@gmail.com>
2021-02-06 21:50:48 -0500

    add corgnaization and project name to docusaurus config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,8 +6,8 @@ module.exports = {
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
   favicon: "img/favicon.ico",
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
+  organizationName: "camilo86", // Usually your GitHub org/user name.
+  projectName: "engl-582-project", // Usually your repo name.
   themeConfig: {
     navbar: {
       title: "ENGL-582 Project",
